e067605a108af366e687c937a6e75bfa283f2baf
AlexWalkerGD <alexwalkerson@hotmail.com>
2025-11-22 23:48:37 +0000

    initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center pt-10">
+      Todos os direitos reservados © AlexWalkerGD
+    </div>
+  );
+};
+
+export default Footer;
